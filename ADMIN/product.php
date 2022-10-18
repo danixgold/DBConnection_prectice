d4be82c7bbca9f8d8185d6291d5c0cde0c50a731
danixgold <114514161+danixgold@users.noreply.github.com>
2022-10-18 12:16:26 -0500

    Add files via upload

---
+++ ADMIN/product.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+include '../Global/ServerConfiguration.php';
+include '../Global/DBConnection.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+    
+</body>
+</html>
---
+++ Global/ServerConfiguration.php
@@ -0,0 +1,11 @@
+<?php
+define("SERVER", "localhost");
+define("USER","root");
+define("PASSWORD","");
+define("DB","abarrotes_javi");
+
+
+
+
+
+?>
